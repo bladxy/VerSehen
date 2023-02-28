e81885b816a60502618bd3c11d324b1767a64ac1
Christopher Jaeger <christopher.jaeger@gds.eu>
2023-02-28 09:28:05 +0100

    verzweiflung schon wieder

---
+++ Core/KiLogics.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using VerSehen.MVVM.ViewModel;
+
+namespace VerSehen.Core
+{
+    public class KiLogics
+    {
+        public HomeViewModel _homeViewModel { get; set; }
+
+        public static void StartKi()
+        {
+        }
+    }
+}
--- MVVM/ViewModel/MainViewModel.cs
+++ MVVM/ViewModel/MainViewModel.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Controls;
 using VerSehen.Core;
 using VerSehen.Services;
 
@@ -22,13 +21,40 @@ namespace VerSehen.MVVM.ViewModel
         }
         public RelayCommand NavigateToHomeCommand { get; set; }
         public RelayCommand NavigateToSettingsCommand { get; set; }
+        public RelayCommand StartKi { get; set; }
 
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
+        [DllImport("user32.dll")]
+        static extern IntPtr SetFocus(IntPtr hWnd);
+
+        public void SetFocusToWinFormsApp()
+        {
+            // Find the ContentPresenter in the MainWindow
+            MainWindow mainWindow = (MainWindow)Application.Current.MainWindow;
+            ContentControl contentControl = (ContentControl)mainWindow.FindName("ContentControl");
+            var homeview = contentControl.Content as HomeViewModel;
+            
+            // Find the Windows Forms host panel
+            var hostPanel = homeview.WfHost.Child;
+
+            // Set focus to the Windows Forms application
+            SetForegroundWindow(hostPanel.Handle);
+            SetFocus(hostPanel.Handle);
+        }
 
         public MainViewModel(INavigationService navigationService)
         {
             Navigation = navigationService;
-            NavigateToHomeCommand = new RelayCommand(o =>{ Navigation.NavigateTo<HomeViewModel>(); }, o => true); 
+            NavigateToHomeCommand = new RelayCommand(o => { Navigation.NavigateTo<HomeViewModel>(); }, o => true);
             NavigateToSettingsCommand = new RelayCommand(o => { Navigation.NavigateTo<SettingsViewModel>(); }, o => true);
+            StartKi = new RelayCommand(o =>
+            {
+                Navigation.NavigateTo<HomeViewModel>();
+                SetFocusToWinFormsApp();
+                KiLogics.StartKi();
+            }, o => true);
         }
     }
 }
