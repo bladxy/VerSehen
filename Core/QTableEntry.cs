9db1933893528da7444e9c4022b3ae879a57f8d6
Christopher Jaeger <christopher.jaeger@gds.eu>
2023-07-25 17:21:12 +0200

    d

--- Core/QTableEntry.cs
+++ Core/QTableEntry.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VerSehen.MVVM.Model;
+using Action = VerSehen.MVVM.Model.Action;
 
 namespace VerSehen.Core
 {
-    internal class QTableEntry
+    public class QTableEntry
     {
+        public State State { get; set; }
+        public Dictionary<Action, double> Actions { get; set; }
     }
 }
